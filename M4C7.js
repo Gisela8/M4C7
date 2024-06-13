e30cbd5f253fad1baa88da9fef728d08fd12542c
gisela_saez_herran <gissahe@yahoo.es>
2024-06-14 01:27:52 +0200

    Add files via upload

---
+++ M4C7.js
@@ -0,0 +1,14 @@
+function operation_mat(n1, n2, n3, n4)
+    
+{  resultado = (n1 + n2) * (n3 + n4);
+ 
+      if(resultado > 50)
+      
+      {return console.log("¡El número es mayor que 50!");} 
+            
+      else {return console.log("¡El número es menor que 50!");}
+   
+}
+
+   operation_mat(2, 2, 1, 1);
+   
